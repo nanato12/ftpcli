569e3a44d3676a296e42494c787817633b5e96b5
nanato12 <kohakujb26@gmail.com>
2019-11-26 18:43:24 +0900

    Add some files ・.gitignore ・setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ftpcli',
+    packages=['ftpcli'],
+
+    version='1.0.1',
+
+    license='MIT',
+
+    install_requires=[],
+
+    author='nanato12',
+    author_email='admin@nanato12.info',
+
+    url='https://github.com/nanato12/ftpcli',
+
+    description='To easy ftp client.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='ftpcli ftp-client',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
